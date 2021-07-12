0d3e5f8cb68337c6039b68fa64dae843edc43819
joy biswas <contact.joybiswas@gmail.com>
2021-07-12 22:09:31 +0600

    Js File added

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function showMenu() {
-    onclick = "document.getElementById('drop').style.display = 'block'"
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
